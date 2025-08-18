301ca86159db0662e97edc7ff6e0ed4d86bd7fcf
siddmazak <sid@akvitek.com.au>
2025-08-18 17:31:32 +0530

    disable eslint and type checking on build for dev

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Disable eslint
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Disable type checking
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
